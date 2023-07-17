d8270a20bb67956c9cad7d95f52a9235540d7a6c
dropperDEV <jamescosta4747@Gmail.com>
2023-07-17 00:44:11 -0300

    challenge 2 v2 finished

--- 2-challenge/src/App.jsx
+++ 2-challenge/src/App.jsx
@@ -3,52 +3,64 @@ import "./App.css";
 
 export default function App() {
   const [step, setStep] = useState(1);
-  const [previousValue, setPreviousStep] = useState(50);
   const [count, setCount] = useState(0);
   const date = new Date("july 14 2023");
   date.setDate(date.getDate() + count);
-
-  function handleRangeChanges(event) {
-    const currentStep = Number(event.target.value);
-    setStep(currentStep);
-
-    if (currentStep > previousValue) {
-      setStep((step) => step + 1);
-    } else if (currentStep < previousValue) {
-      setStep((step) => step + 1);
-      
-    }
-    setPreviousStep(currentStep);
+  function handleClickReset() {
+    setStep(1);
+    setCount(0);
   }
 
-    return (
-      <div className="container">
-        <div className="top-wrapper">
-          <input
-            type="range"
-            min={1}
-            max={10}
-            value={step}
-            onChange={handleRangeChanges}
-          />
-          <label htmlFor="range">{step}</label>
-        </div>
-        <div className="top-wrapper">
-          <button onClick={() => setCount((count) => count - 1)}>➖</button>
-          <input type="text" value={count} />
-          <button onClick={() => setCount((count) => count + 1)}>➕</button>
-        </div>
-        <p>
-          <span>
-            {count === 0
-              ? "Today is "
-              : count > 0
-              ? `${count} days from today `
-              : `${count} days ago was `}
-          </span>
-          <span>{date.toDateString()}</span>
-        </p>
+  return (
+    <div className="container">
+      <div className="top-wrapper">
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <label htmlFor="range">{step}</label>
       </div>
-    );
-  }
-
+      <div className="top-wrapper">
+        <button
+          onClick={() => setCount((count) => count - 1 * step)}
+          className="button"
+        >
+          ➖
+        </button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) =>
+            setCount(
+              Number(e.target.value) >= 0
+                ? parseInt(e.target.value)
+                : -parseInt(e.target.value)
+            )
+          }
+        />
+        <button
+          onClick={() => setCount((count) => count + 1 * step)}
+          className="button"
+        >
+          ➕
+        </button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today `
+            : `${count} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      <button className="button" onClick={handleClickReset}>
+        Reset
+      </button>
+    </div>
+  );
+}
