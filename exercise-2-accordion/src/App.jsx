16d79ab0f572f05845efffe16d14ccd0811ab063
dropperDEV <jamescosta4747@Gmail.com>
2023-07-22 17:08:20 -0300

    exercise accordion with children props

--- exercise-2-accordion/src/App.jsx
+++ exercise-2-accordion/src/App.jsx
@@ -13,7 +13,6 @@ const faqs = [
     title: "Do you ship to countries outside the EU?",
     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
   },
-  
 ];
 
 export default function App() {
@@ -25,26 +24,53 @@ export default function App() {
 }
 
 function Accordion() {
+  const [curOpen, setCurOpen] = useState(0);
+
   return (
     <div className="accordion">
       {faqs.map((item, i) => (
-        <ItemsAcc title={item.title} num={i+1} text={item.text} key={item.title} />
+        <ItemsAcc
+          setCurOpen={setCurOpen}
+          title={item.title}
+          num={i + 1}
+          key={item.title}
+          curOpen={curOpen}
+        >
+          {item.text}
+        </ItemsAcc>
       ))}
+      <ItemsAcc
+        setCurOpen={setCurOpen}
+        title="Simplesmente Miau"
+        num="DEZ"
+        curOpen={curOpen}
+      >
+        <ul>
+          <li>Primeiro. Seja um gato(a)</li>
+          <li>Segundo. Sinta vontade de miar</li>
+          <li>Terceiro. Mie</li>
+        </ul>
+      </ItemsAcc>
     </div>
   );
 }
 
-function ItemsAcc({num,title, text}) {
-  const [isOpen, setIsOpen] = useState(false)
-  function handleClickisOpen(){
-    setIsOpen(isOpen => !isOpen)
+function ItemsAcc({ num, title, setCurOpen, curOpen, children }) {
+  const isOpen = num === curOpen;
+  function handleToggle() {
+    setCurOpen(num);
   }
   return (
-    <div className={isOpen === false ? "item" : "item open"} onClick={handleClickisOpen}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num}</p>
       <h1 className="title">{title}</h1>
       <span className="icon">{isOpen === false ? "➖" : "➕"}</span>
-      <div style={isOpen === false ? { display: "none" }: {}} className="content-box">{text}</div>
+      <div
+        style={isOpen === false ? { display: "none" } : {}}
+        className="content-box"
+      >
+        {children}
+      </div>
     </div>
   );
 }
