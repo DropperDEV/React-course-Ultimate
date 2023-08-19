88ad84fd791eb0c345767dc13f5f508ebcc72435
dropperDEV <jamescosta4747@Gmail.com>
2023-08-19 20:10:27 -0300

    fake api incorporated with json-server questions added

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,41 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
-export default function App(){
-  return <div>
-    <DateCounter/>
-  </div>
+const intialState = {
+  questions: [],
+  // 'loading','error','ready','active','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [ state, dispatch]  = useReducer(reducer, intialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main></Main>
+    </div>
+  );
 }
