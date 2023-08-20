bb1ffa325b4b12af7fde3871657b9e00047d6fbd
dropperDEV <jamescosta4747@Gmail.com>
2023-08-20 10:53:08 -0300

    handling new answer

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -4,14 +4,16 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
-import Question from './Question';
+import Question from "./Question";
 
 const intialState = {
   questions: [],
   // 'loading','error','ready','active','finished'
   status: "loading",
 
-  index: 0
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -24,16 +26,29 @@ function reducer(state, action) {
       };
     case "dataFailed":
       return { ...state, status: "error" };
-      case "start":
+    case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions[state.index];
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [ {questions,index,status}, dispatch]  = useReducer(reducer, intialState);
-  const numQuestions = questions.length
+  const [{ questions, index, status, answer }, dispatch] = useReducer(
+    reducer,
+    intialState
+  );
+  const numQuestions = questions.length;
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -45,10 +60,18 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        {status === 'loading' && <Loader/>}
-        {status === 'error' && <Error/>}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-        {status === 'active' && <Question question={questions[index]}/>}
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
