db6b19040598d79f6e712a64a10baacb72f2f6c8
dropperDEV <jamescosta4747@Gmail.com>
2023-08-19 23:05:50 -0300

    splited option component

---
+++ react-quiz/src/components/Option.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function Option({question}) {
+  return (
+    <div className="options">
+        {question.options.map((option) => (
+          <button className="btn btn-option" key={option}>
+            {option}
+          </button>
+        ))}
+      </div>
+  )
+}
--- react-quiz/src/components/Question.jsx
+++ react-quiz/src/components/Question.jsx
@@ -1,17 +1,12 @@
 import React from "react";
+import Option from "./Option";
 
 export default function Question({ question }) {
   console.log(question)
   return (
     <div>
       <h4>{question.question}</h4>
-      <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
-            {option}
-          </button>
-        ))}
-      </div>
+      <Option question={question}/>
     </div>
   );
 }
