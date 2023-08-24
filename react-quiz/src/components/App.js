b8cc4bff681fa9d6367ae9d07875904217a3e55a
dropperDEV <jamescosta4747@Gmail.com>
2023-08-23 22:55:57 -0300

    Timer finished

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -9,6 +9,9 @@ import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
 import RestartButton from "./RestartButton";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const intialState = {
   questions: [],
@@ -19,6 +22,7 @@ const intialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -32,7 +36,11 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
     case "newAnswer":
       const question = state.questions[state.index];
       return {
@@ -53,15 +61,23 @@ function reducer(state, action) {
           state.points > state.highscore ? state.points : state.highscore,
       };
     case "restart":
-      return { ...state, status: "ready", index: 0, points: 0, answer: null };
+      return { intialState, status: "ready", highscore: state.highscore };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, index, status, answer, points, highscore }, dispatch] =
-    useReducer(reducer, intialState);
+  const [
+    { questions, index, status, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, intialState);
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
     0
@@ -103,6 +119,7 @@ export default function App() {
               index={index}
               numQuestions={numQuestions}
             />
+            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
           </>
         )}
         {status === "finished" && (
