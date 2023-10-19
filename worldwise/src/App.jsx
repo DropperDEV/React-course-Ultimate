25d7576558fabe4b9423f376d851b02d560a9250
dropperDEV <jamescosta4747@Gmail.com>
2023-10-18 22:31:11 -0300

    multiples class module

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/pageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
@@ -10,6 +12,9 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        
+        <Route path="*" element={<PageNotFound/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- worldwise/src/pages/Homepage.jsx
+++ worldwise/src/pages/Homepage.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+import AppNav from './../components/AppNav';
 
 export default function Homepage() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <PageNav/>
+      <AppNav/>
+      <h1>HomePage</h1>
+      <Link to="/app">Go to the app</Link>
+    </div>
+  );
 }
