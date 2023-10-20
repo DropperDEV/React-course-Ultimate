ede890932c1b30cd78cb0e8147d9eef4da47f886
dropperDEV <jamescosta4747@Gmail.com>
2023-10-19 23:47:20 -0300

    started app layout

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,13 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/SideBar";
+import Map from "../components/Map";
+
+import styles from './AppLayout.module.css'
 
 export default function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <h1>App</h1>
+    <div className={styles.app}>
+      <Sidebar/>
+      <Map/>
     </div>
   );
 }
