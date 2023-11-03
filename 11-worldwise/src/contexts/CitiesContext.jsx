0223a65b7d63abe4f37c01cbf9d938d4250a51b3
dropperDEV <jamescosta4747@Gmail.com>
2023-11-03 10:30:57 -0300

    useReducer in cities context

--- 11-worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -1,27 +1,70 @@
 /* eslint-disable react/prop-types */
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 
 const CitiesContext = createContext();
 
 const BASE_URL = "http://localhost:9000";
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: ""
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "Loading":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+    case "cities/created":
+
+    case "cities/deleted":
+
+    case "rejected":
+      return {
+        ...state, isLoading: false, error: action.payload
+      }
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ isLoading, cities, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
+      dispatch({ type: "loading" });
+
       try {
-        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCities(data);
+        dispatch({type: "cities/loaded", payload: data})
       } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
+        dispatch({type: "rejected",payload: "There was an error loading data..."});
+      } 
     }
     fetchCities();
   }, []);
