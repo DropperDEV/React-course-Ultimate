fc6d5b1e6b11f5079b10d8d2c04e5b896f8d9f83
dropperDEV <jamescosta4747@Gmail.com>
2023-11-08 19:53:03 -0300

    finished 19 optimization & a.effect

--- challenges/7-useMemoChallenge/src/Calculator.js
+++ challenges/7-useMemoChallenge/src/Calculator.js
@@ -1,41 +1,23 @@
-import { useCallback, useEffect, useState } from "react";
-import clickSound from "./ClickSound.m4a";
-import { memo } from "react";
+import { useState } from 'react';
+import clickSound from './ClickSound.m4a';
+import { memo } from 'react';
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
-  const [duration, setDuration] = useState(0);
+
+  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  useEffect(
-    function () {
-      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
-    },
-    [number, sets, speed, durationBreak]
-  );
-  useEffect(
-    function () {
-      playSound();
-    },
-    [duration]
-  );
-
-  function playSound() {
+  const playSound = function () {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
-  }
+  };
 
-  function handleInc() {
-    setDuration((duration) => Math.floor(duration) + 1);
-  }
-  function handleDec() {
-    setDuration((duration) => duration && Math.floor(duration) - 1);
-  }
   return (
     <>
       <form>
@@ -52,9 +34,9 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>How many sets?</label>
           <input
-            type="range"
-            min="1"
-            max="5"
+            type='range'
+            min='1'
+            max='5'
             value={sets}
             onChange={(e) => setSets(e.target.value)}
           />
@@ -63,10 +45,10 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>How fast are you?</label>
           <input
-            type="range"
-            min="30"
-            max="180"
-            step="30"
+            type='range'
+            min='30'
+            max='180'
+            step='30'
             value={speed}
             onChange={(e) => setSpeed(e.target.value)}
           />
@@ -75,9 +57,9 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>Break length</label>
           <input
-            type="range"
-            min="1"
-            max="10"
+            type='range'
+            min='1'
+            max='10'
             value={durationBreak}
             onChange={(e) => setDurationBreak(e.target.value)}
           />
@@ -85,13 +67,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={handleDec}>–</button>
+        <button onClick={() => {}}>–</button>
         <p>
-          {mins < 10 && "0"}
-          {mins}:{seconds < 10 && "0"}
+          {mins < 10 && '0'}
+          {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={handleInc}>+</button>
+        <button onClick={() => {}}>+</button>
       </section>
     </>
   );
