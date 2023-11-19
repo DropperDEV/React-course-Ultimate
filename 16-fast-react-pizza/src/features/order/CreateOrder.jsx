1e7f82c35f7db9258be7ad0fccd0b1211d439027
dropperDEV <jamescosta4747@Gmail.com>
2023-11-19 18:54:40 -0300

    reusing components css classnames with gambiarra

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,6 +3,7 @@
 
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "./../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -62,12 +63,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-              className=" input"
-            />
+            <input type="text" name="address" required className=" input" />
           </div>
         </div>
 
@@ -85,12 +81,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="focus:ring-2- inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300  focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2  disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
