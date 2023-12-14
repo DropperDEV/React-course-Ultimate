4ce3a6bb4eb97ed09d250cf75b0887d141a344db
dropperDEV <jamescosta4747@Gmail.com>
2023-12-13 22:42:34 -0300

    feat: CartItem finished style

--- 16-fast-react-pizza/src/features/cart/CartItem.jsx
+++ 16-fast-react-pizza/src/features/cart/CartItem.jsx
@@ -1,14 +1,18 @@
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
+
 /* eslint-disable react/prop-types */
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+  const { name, quantity, totalPrice } = item;
 
   return (
-    <li>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p>
         {quantity}&times; {name}
       </p>
-      <div>
+      <div className="flex justify-between items-center sm:gap-6">
         <p>{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
