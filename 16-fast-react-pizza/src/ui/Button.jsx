31e4f752fa9adece7c02751e407c29c286d09d57
dropperDEV <jamescosta4747@Gmail.com>
2023-12-13 22:23:18 -0300

    feat: menu finished tailwind

--- 16-fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/src/ui/Button.jsx
@@ -2,12 +2,23 @@
 
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled,to }) {
-  const classname =
-    "focus:ring-2- inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring  focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6  sm:py-4";
-  if(to) return <Link to={to} className={classname}>{children}</Link>
+export default function Button({ children, disabled, to, type }) {
+  const base =
+    "focus:ring-2- inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring  focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+
+  const styles = {
+    primary: base + " px-4 py-3 sm:px-6  sm:py-4",
+    small: base + " px-4 py-2 sm:px-5  sm:py-2.5 text-xs",
+
+  };
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
   return (
-    <button disabled={disabled} className={classname}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
