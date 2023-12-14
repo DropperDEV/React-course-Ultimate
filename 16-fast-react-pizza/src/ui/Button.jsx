2da7d4c7909d8966987b3d34665b4dc88dbab244
dropperDEV <jamescosta4747@Gmail.com>
2023-12-13 23:30:22 -0300

    feat: createOrder stylized

--- 16-fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/src/ui/Button.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "focus:ring-2- inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring  focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+    "focus:ring-2- text-sm inline-block rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring  focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
   const styles = {
     primary: base + " px-4 py-3 sm:px-6  sm:py-4",
     small: base + " px-4 py-2 sm:px-5  sm:py-2.5 text-xs",
-    secondary:     "focus:ring-2- inline-block rounded-full border-2 border-stone-300  font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-800  focus:outline-none focus:ring  focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 sm:px-6  sm:py-3.5 "
+    secondary:     "focus:ring-2- text-sm inline-block rounded-full border-2 border-stone-300  font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-800  focus:outline-none focus:ring  focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 sm:px-6  sm:py-3.5 "
 
 
   };
