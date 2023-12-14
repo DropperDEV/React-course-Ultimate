22b25c469365c5cff796b054254e2b3d7c166d49
dropperDEV <jamescosta4747@Gmail.com>
2023-12-14 20:08:55 -0300

    feat: CartOverview linked cartslice

--- 16-fast-react-pizza/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/src/features/cart/cartSlice.js
@@ -2,13 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
   ],
 };
 
@@ -50,4 +43,12 @@ export const {
   clearCart,
 } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+  export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+// reselect library
