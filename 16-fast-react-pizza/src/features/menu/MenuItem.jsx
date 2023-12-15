f8ca27e8297f0a5f9b81a7cef54d9ea1595da98c
dropperDEV <jamescosta4747@Gmail.com>
2023-12-15 15:21:15 -0300

    feat: Updating cart

--- 16-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -5,6 +5,7 @@ import Button from "./../../ui/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 export default function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -43,7 +44,12 @@ export default function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {IsInCart && <DeleteItem pizzaId={id} />}
+          {IsInCart && (
+            <div className="flex items-center gap-3 sm:gap-3">
+              <UpdateItemQuantity pizzaId={id} currentQuantity={currentQuantity} />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
           {!soldOut && !IsInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
