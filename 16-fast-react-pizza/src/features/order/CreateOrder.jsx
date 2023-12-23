ef183fba447621452eee9065c04476a3436635ad
dropperDEV <jamescosta4747@Gmail.com>
2023-12-23 16:05:41 -0300

    feat: thunk geolocation

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -4,12 +4,13 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "./../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "./../cart/cartSlice";
 import EmptyCart from "./../cart/EmptyCart";
 import store from "./../../store";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
+import { fetchAdress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -24,10 +25,10 @@ function CreateOrder() {
   const formErrors = useActionData();
   const cart = useSelector(getCart);
   const username = useSelector((state) => state.user.username);
-
-  const totalCartPrice = useSelector(getTotalCartPrice)
-  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0
-  const totalPrice = totalCartPrice + priorityPrice
+  const dispatch = useDispatch();
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
 
   if (!cart.length) return <EmptyCart />;
 
@@ -69,6 +70,9 @@ function CreateOrder() {
               className=" input w-full"
             />
           </div>
+          <Button type="small" onClick={() => dispatch(fetchAdress())}>
+            Get position
+          </Button>
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -88,7 +92,9 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? "Placing order..." : `Order now from ${formatCurrency(totalPrice)}`}
+            {isSubmitting
+              ? "Placing order..."
+              : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
